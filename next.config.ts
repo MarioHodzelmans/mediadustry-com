392e9ebfe50d7611f4c725eb06e6a3f35c9cf3db
Mario Hodzelmans <mariohodzelmans@MBP-van-Mario.localdomain>
2026-09-17 01:07:38 +0200

    feat: integrate purchased Agntix Creative Studio template

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {remotePatterns: [{protocol: "https", hostname: "cdn.shopify.com"}]},
+  turbopack: {},
 };
 
 export default nextConfig;
